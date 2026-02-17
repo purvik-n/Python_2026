2d1863c8f18bfbbbcc4ed5b6b609ea9fd87fcb47
purvik-n <purviknarasimha20@gmail.com>
2026-02-17 22:15:06 +0530

    Add random friend picker program

---
+++ task12.py
@@ -0,0 +1,7 @@
+import random
+friends = ["Aman", "Rahul", "Sonia", "Priya", "Amit"]
+# 1 option
+print(random.choice(friends))
+# 2 option 
+rendom_index = random.randint(0,4)
+print(friends[rendom_index])
