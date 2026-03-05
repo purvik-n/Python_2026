1298a87868d2959cc1ad3d4ddfd349a6c08c51d0
purvik-n <purviknarasimha20@gmail.com>
2026-03-05 21:46:05 +0530

    refactor: optimize function task structure

--- function_task.py
+++ function_task.py
@@ -1,6 +1,6 @@
 def greet_with_name(name):
     print(f"Hello, {name}!")
-    print("How are you "{name}"?")
-    print("Isn't the weather nice today" {name} "?")
+    print("How are you,?")
+    print("Isn't the weather nice today?")
     
 greet_with_name("purvik")
