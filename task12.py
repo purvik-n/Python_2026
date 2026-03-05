351674959201b25ac0ac658eddb55d81e7ac184e
purvik-n <purviknarasimha20@gmail.com>
2026-03-05 21:46:06 +0530

    docs: add comments to random choice logic

--- task12.py
+++ task12.py
@@ -1,6 +1,8 @@
 import random
+# Defining a list of friends for random selection
 friends = ["Aman", "Rahul", "Sonia", "Priya", "Amit"]
 # 1 option
+# Directly using random choice
 print(random.choice(friends))
 # 2 option 
 rendom_index = random.randint(0,4)
